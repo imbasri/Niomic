d1e8a4815eca7fa6500ac60930b4ec93d63500c6
imbasri12 <imbasri12@gmail.com>
2021-11-10 16:10:06 +0700

    tugas 2 javascript tipe data

---
+++ Niomic/javascript/tugas2.js
@@ -0,0 +1,12 @@
+/*Buatlah 5 buah variabel dengan 5 tipe data yang berbeda number , string , boolean , null atau undifined dan array
+Didalam variabel tersebut teman teman isikan nilai yang berbeda tiap variabelnya
+Setelah itu teman teman tampilkan kelima variabel tersebut dengan console.log() */
+let number = 24; //tipe data number
+let string = "Niomic"; //tipe data string
+let boolean = true; // tipe data boolean true or false
+let variabel = null; //tipe data null
+let iniVariabel; //undifined
+let iniArray = [1, 2, 3, 4, 5];
+
+console.log(number, string, boolean, variabel, iniVariabel, iniArray);
+//cara pemanggilan print ke console
